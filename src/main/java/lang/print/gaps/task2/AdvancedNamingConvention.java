0dfd2fba1e8afa8387544ab83a95936420da5611
Mykty <baha@Bahas-MacBook-Pro.local>
2022-08-09 14:40:51 +0600

    final commit

--- src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/AdvancedNamingConvention.java
@@ -1,21 +1,18 @@
 package lang.print.gaps.task2;
 
 public class AdvancedNamingConvention {
+
     private static final int ADULT_AGE = 18;
 
     private int age;
     private int phoneNumber;
 
-
     void callToFriend() {
         //some code
-        age = 19;
-        System.out.println(ADULT_AGE+" year old!");
     }
 
     void callByNumber(int number) {
         //some code
-        phoneNumber = number;
-        System.out.println("Phone number is: "+phoneNumber);
     }
+
 }
--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -4,11 +4,15 @@ public class CopiedNames {
     public static void main(String[] args) {
         int variable = 0;
         int variable2 = 2000;
-        int variable3 = 4000;
-        int variables = 5000;
         int value = 1000;
+        int variable3 = 4000;
         int value2 = 3000;
+        int variables = 5000;
         variables = 7000;
         int value3 = 90000;
+        int integer = 100000;
+        int myInteger = 1000000;
+        int myVar = 1234567;
+        int myInteger2 = 1234567;
     }
 }
